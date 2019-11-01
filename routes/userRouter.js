19e872f6084db789cac17c9dc52f8488fcd44d42
Dan Clayton <clayton.daniel.j@gmail.com>
2019-11-01 16:02:51 -0600

    user router that does nothing

--- routes/userRouter.js
+++ routes/userRouter.js
@@ -2,15 +2,6 @@ const express = require('express')
 const userRouter = express.Router()
 const User = require('../models/user.js')
 
-userRouter.route('/')
-    .get((req, res, next) => {
-        User.find((err, allUsers) => {
-            if (err) {
-                res.status(500);
-                return next(err);
-            }
-            return res.status(200).send(allUsers);
-        })
-    })
+//hello!
 
 module.exports = userRouter;
